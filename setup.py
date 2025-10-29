491c60fe618aefa6c193d491cedfae7bbe27aae9
Liang Liu <liangliu@fnal.gov>
2025-10-29 17:19:46 -0500

    cleaning up

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ccxp",
-    version="0.1.0",
-    packages=find_packages(),   # automatically finds 'my_project'
-    python_requires=">=3.8",
-)
